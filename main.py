ca99cfa1dafcf6b53c20c664cc6374bf497bfe11
Jacob Libby <jacobmlibby@gmail.com>
2026-08-29 13:01:17 -0500

    updating kivy progress

--- main.py
+++ main.py
@@ -2,6 +2,54 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.stacklayout import StackLayout
+from kivy.metrics import dp
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.pagelayout import PageLayout
+
+class PageLayoutExample(PageLayout):
+    pass
+
+class ScrollViewExample(ScrollView):
+
+    pass
+
+class StackLayoutExample(StackLayout):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        self.orientation = "lr-tb"
+        for i in range(0,100):
+            #size = dp(100)
+            #b = Button(text=str(i+1),size_hint=(None,None),size=(size,size))
+            b = Button(text=str(i+1),size_hint=(1,None),size=(1,dp(100)))
+            self.add_widget(b)
+
+    pass
+
+
+class GridLayoutInfoList(GridLayout):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        self.cols=1
+        for i in range(0,100):
+            size = dp(100)
+            b = Button(text=str(i+1),size_hint=(None,None),size=(size,size))
+            self.add_widget(b)
+
+    pass
+
+
+# class GridLayoutExample(GridLayout):
+#     pass
+
+class AnchorLayoutExample(AnchorLayout):
+    pass
+
+
+
+
 
 class BoxLayoutExample(BoxLayout):
 
