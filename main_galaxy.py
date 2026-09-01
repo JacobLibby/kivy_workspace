b7149eaa49c41b7b9fba08824f6cb94b0da8068f
Jacob Libby <jacobmlibby@gmail.com>
2026-08-31 19:42:23 -0500

    proportions for x and y transform

--- main_galaxy.py
+++ main_galaxy.py
@@ -8,6 +8,9 @@ class MainWidget(Widget):
     perspective_point_x = NumericProperty(0)
     perspective_point_y = NumericProperty(0)
 
+    V_NB_LINES = 7
+    V_LINES_SPACING = .1
+    vertical_lines = []
     line = None
 
     def __init__(self,**kwargs):
@@ -35,12 +38,40 @@ class MainWidget(Widget):
     def init_vertical_lines(self):
         with self.canvas:
             Color(1,1,1)
-            self.line = Line(points=[100,0,100,100])
+            # self.line = Line(points=[100,0,100,100])
+            for i in range(0, self.V_NB_LINES):
+                self.vertical_lines.append(Line())
 
     def update_vertical_lines(self):
-        center_x = int(self.width/2)
-        #center_y = int(self.height*.75)
-        self.line.points = [center_x,0,center_x,100]
+        central_line_x = int(self.width/2)
+        offset = -int(self.V_NB_LINES/2)
+        spacing = self.V_LINES_SPACING * self.width
+        for i in range(0, self.V_NB_LINES):
+            line_x = int(central_line_x + offset*spacing)
+            x1, y1 = self.transform(line_x, 0)
+            x2, y2 = self.transform(line_x, self.height)
+            self.vertical_lines[i].points = [x1, y1, x2, y2]
+            offset += 1
+
+    def transform(self, x, y):
+        # return self.transform_2D(self, x, y)
+        return self.transform_perspective(x, y)
+
+
+    def transform_2D(self, x, y):
+        return int(x), int(y)
+
+    def transform_perspective(self, x, y):
+        tr_y = y * self.perspective_point_y / self.height
+        if tr_y > self.perspective_point_y:
+            tr_y = self.perspective_point_y
+
+        diff_x = x - self.perspective_point_x
+        diff_y = self.perspective_point_y - tr_y
+        proportion_y = diff_y/self.perspective_point_y
+
+        tr_x = self.perspective_point_x + diff_x*proportion_y
+        return int(tr_x), int(tr_y)
 
 
 class GalaxyApp(App):
