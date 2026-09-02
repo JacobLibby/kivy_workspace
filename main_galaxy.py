1ed686c7a12bbad9a85330c3ef9fed2c02761133
Jacob Libby <jacobmlibby@gmail.com>
2026-09-02 18:22:57 -0500

    updating proportion of visualization of galaxy

--- main_galaxy.py
+++ main_galaxy.py
@@ -8,14 +8,20 @@ class MainWidget(Widget):
     perspective_point_x = NumericProperty(0)
     perspective_point_y = NumericProperty(0)
 
-    V_NB_LINES = 7
-    V_LINES_SPACING = .1
+    V_NB_LINES = 10
+    V_LINES_SPACING = .25 # percentage of screen width
     vertical_lines = []
     line = None
+
+    H_NB_LINES = 15
+    H_LINES_SPACING = .1 # percentage of screen height
+    horizontal_lines = []
 
     def __init__(self,**kwargs):
         super(MainWidget, self).__init__(**kwargs)
         self.init_vertical_lines()
+        self.init_horizontal_lines()
+
         # print(f"INIT Width: {str(self.width)}, INIT Height: {str(self.height)}")
 
     def on_parent(self, widget, parent):
@@ -26,6 +32,7 @@ class MainWidget(Widget):
         # self.perspective_point_x = self.width/2
         # self.perspective_point_y = self.height*.75
         self.update_vertical_lines()
+        self.update_horizontal_lines()
 
     def on_perspective_point_x(self,widget,value):
         # print(f"PERSPECTIVE X: {str(value)}")
@@ -42,19 +49,42 @@ class MainWidget(Widget):
             for i in range(0, self.V_NB_LINES):
                 self.vertical_lines.append(Line())
 
+    def init_horizontal_lines(self):
+        with self.canvas:
+            Color(1,1,1)
+            # self.line = Line(points=[100,0,100,100])
+            for i in range(0, self.H_NB_LINES):
+                self.horizontal_lines.append(Line())
+
     def update_vertical_lines(self):
         central_line_x = int(self.width/2)
-        offset = -int(self.V_NB_LINES/2)
         spacing = self.V_LINES_SPACING * self.width
+        offset = -int(self.V_NB_LINES/2)+0.5
         for i in range(0, self.V_NB_LINES):
             line_x = int(central_line_x + offset*spacing)
+
             x1, y1 = self.transform(line_x, 0)
             x2, y2 = self.transform(line_x, self.height)
             self.vertical_lines[i].points = [x1, y1, x2, y2]
             offset += 1
 
+    def update_horizontal_lines(self):
+        central_line_x = int(self.width/2)
+        spacing = self.V_LINES_SPACING * self.width
+        offset = int(self.V_NB_LINES/2)-0.5
+
+        xmin = central_line_x-offset*spacing
+        xmax = central_line_x+offset*spacing
+        spacing_y = self.H_LINES_SPACING*self.height
+        for i in range(0, self.H_NB_LINES):
+            line_y = i*spacing_y
+
+            x1, y1 = self.transform(xmin, line_y)
+            x2, y2 = self.transform(xmax, line_y)
+            self.horizontal_lines[i].points = [x1, y1, x2, y2]
+
     def transform(self, x, y):
-        # return self.transform_2D(self, x, y)
+        # return self.transform_2D(x, y)
         return self.transform_perspective(x, y)
 
 
@@ -62,15 +92,17 @@ class MainWidget(Widget):
         return int(x), int(y)
 
     def transform_perspective(self, x, y):
-        tr_y = y * self.perspective_point_y / self.height
-        if tr_y > self.perspective_point_y:
-            tr_y = self.perspective_point_y
+        lin_y = y * self.perspective_point_y / self.height
+        if lin_y > self.perspective_point_y:
+            lin_y = self.perspective_point_y
 
         diff_x = x - self.perspective_point_x
-        diff_y = self.perspective_point_y - tr_y
-        proportion_y = diff_y/self.perspective_point_y
+        diff_y = self.perspective_point_y - lin_y
+        factor_y = diff_y/self.perspective_point_y
+        factor_y = factor_y * factor_y
 
-        tr_x = self.perspective_point_x + diff_x*proportion_y
+        tr_x = self.perspective_point_x + diff_x*factor_y
+        tr_y = self.perspective_point_y - self.perspective_point_y*factor_y
         return int(tr_x), int(tr_y)
 
 
